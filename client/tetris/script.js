d88c0e4185dfb7b2d35461bb08b04ba8d4280ffa
John Murray <69658747+lovendatj@users.noreply.github.com>
2022-02-17 21:17:16 -0500

    updating shapes

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,92 +1,96 @@
 // DOM Event Listener
 document.addEventListener('DOMContentLoaded', () => {
-    const [grid, squares] = createSquareInGrid(153, 'grid');
+    const squares = document.querySelectorAll('.grid div');
     const scoreDisplay = document.querySelector('#score');
     const startBtn = document.querySelector('#start-button');
-    const width = 10;
+    let width = 10;
 
     // Create Shapes
     const theTetrominoes = createShapes(width);
 
     // Draw Shapes
     let currentPosition = 4;
-    const [shape_index, random_index] = randomPos(theTetrominoes.length, currentPosition)
-    let current = theTetrominoes[shape_index][random_index];
+    let currentRotation = 1;
+    let shape_index= randomShape(theTetrominoes.length)
+
+    _log(`shape: ${shape_index} rotation: ${currentRotation}`)
+
+    let timerId = setInterval(moveDown, 1000);
+
+    function moveDown(){
+        undrawShapes();
+        currentPosition += width;
+        drawShapes();
+    }
+
+    function undrawShapes() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino');
+        });
+    }
+
+    function drawShapes (offset)  {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino');
+        });
+    }
+
+
+    const current = theTetrominoes[5][1];
+    function createShapes (width)  {
+        return [
+            // The T Shape
+            [
+                [width-1, width, width+1, width*2],
+                [0, width, width+1, width*2],
+                [0, width-1, width, width+1],
+                [0, width-1, width, width*2]
+            ],
+            // The Square 
+            [
+                [width-1, width,width*2-1,width*2],
+                [width-1, width,width*2-1,width*2],
+                [width-1, width,width*2-1,width*2],
+                [width-1, width,width*2-1,width*2]
+            ],
+            // The L Shape
+            [
+                [width,width+1,width+2,width*2],
+                [width+1, width*2-1,width*2,width*2+1],
+                [0,1,width+1,width*2+1],
+                [width-1, width,width+1,width*2-1]
+            ],
+            // The S Shape
+            [
+                [0,1,width-1,width],
+                [0,width,width+1,width*2+1],
+                [0,1,width-1,width],
+                [0,width,width+1,width*2+1],
+            ],
+            // The Line Shape
+            [
+                [0,1,2,3],
+                [0,width, width*2,width*3],
+                [0,1,2,3],
+                [0,width, width*2,width*3]
+            ],
+            // The Z Shape
+            [
+                [0,1,width+1,width+2],
+                [1,width+1,width,width*2],
+                [0,1,width+1,width+2],
+                [1,width+1,width,width*2]
+            ]
+        ]
+    }
+
+    function randomShape (shape_len)   {
+        return Math.floor(Math.random()*shape_len)
+    }
+
+    function _log (message)  {
+        console.log(message);
+    }
     
-    drawShapes(current, currentPosition, squares);
 });
 
-const drawShapes = (current, currentPosition, squares) => {
-    current.forEach(index => {
-        squares[currentPosition + index].classList.add('tetromino');
-    })
-}
-
-const createSquareInGrid = (numbers, parent) => {
-    console.log('Creating Grid');
-    let grid = document.querySelector('.grid');
-    for (let i = 0; i < numbers; i++) {
-        var square = document.createElement('div');
-        grid.appendChild(square);
-    }
-    let squares = Array.from(document.querySelectorAll('.grid div'));
-    return [grid, squares];
-}
-
-const createShapes = (width) => {
-    return [
-        // The L Shape
-        [
-            [1, width+1, width*2, 2],
-            [width, width+1, width+2, width*2+1],
-            [2, width+1, width*2+1, width*2],
-            [width+1, width*2, width*2+1, width*2 +2]
-        ],
-        // The Square 
-        [
-            [width+1, width+2,width*2,width*2+1],
-            [width+1, width+2,width*2,width*2+1],
-            [width+1, width+2,width*2,width*2+1],
-            [width+1, width+2,width*2,width*2+1]
-        ],
-        // The T Shape
-        [
-            [width,width+1,width+2,width*2],
-            [width+1, width*2,width*2+1,width*3-1],
-            [width+1,width*2-1,width*2,width*2+1],
-            [width+1, width*2-1,width*2,width*3-1]
-        ],
-        // The S Shape
-        [
-            [width,width+1,width*2-2,width*2-1],
-            [1,width,width+1,width*2],
-            [width,width+1,width*2-2,width*2-1],
-            [1,width,width+1,width*2]
-        ],
-        // The Line Shape
-        [
-            [width-1,width,width+1,width+2],
-            [1,width, width*2-1,width*3-2],
-            [width-1,width,width+1,width+2],
-            [1,width, width*2-1,width*3-2]
-        ],
-        // The Z Shape
-        [
-            [width,width+1,width*2,width*2+1],
-            [2,width,width+1,width*2-1],
-            [width,width+1,width*2,width*2+1],
-            [2,width,width+1,width*2-1]
-        ]
-    ]
-}
-
-const randomPos = (shape_len, rotation)  => {
-    return [
-        Math.floor(Math.random()*shape_len),
-        Math.floor(Math.random()*rotation)
-    ]
-}
-
-const _log = (message) => {
-    console.log(message);
-}
