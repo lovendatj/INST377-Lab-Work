c48c83707e483a3906ef60bbffa8ac77bc9d5688
John Murray <69658747+lovendatj@users.noreply.github.com>
2022-03-11 22:32:27 -0500

    updated lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,21 +1,44 @@
-const dataHandler = (data) => {
-  const form = document.querySelector('.main_form ');
-  const form_button = document.querySelector('.main_form > .submit');
-  form_button.style.display = 'none';
+async function mainEvent() { // the async keyword means we can make API requests
+  const form = document.querySelector('.main-form');
+  const submit = document.querySelector('.submit');
+
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    
 
-};
-async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    dataHandler(arrayFromJson.data);
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
+  if (arrayFromJson.data.length > 0) {
+    console.log(arrayFromJson.data);
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); 
+      submit.style.display = 'block';
+      dataHandler(arrayFromJson.data);
+    });
+  }
+}
+
+const dataHandler = (data) => {
+  console.log('dataHandler');
+  const randIntInclusive = (min, max) => { return Math.floor(Math.random() * (max - min + 1)) + min; };
+  const range = [ ... Array(15).keys() ];
+  const restList = document.querySelector('.resto-list');
+  const restElement = document.querySelectorAll('.resto-element');
+  restElement.forEach(element => {
+    element.remove();
+  });
+
+
+  range.forEach((i) => {
+    const randomIndex = randIntInclusive(0, data.length - 1);
+    const randomItem = data[randomIndex];
+    const newItem = document.createElement('li');
+    // Add class name to newItem
+    newItem.className = 'resto-element';
+    newItem.innerHTML = `
+      <li>
+        <h3>${randomItem.name}</h3>
+      </li>
+    `;
+    restList.appendChild(newItem);
   });
 
 }
